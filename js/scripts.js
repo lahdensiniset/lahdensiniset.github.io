6fee69ea458d64710abb1b881d9620ede38290cd
Mikael Pernu <mikael.pernu@gmail.com>
2015-04-14 23:41:00 +0300

    Added js into job listing links

--- js/scripts.js
+++ js/scripts.js
@@ -50,6 +50,105 @@ $( document ).ready( function() {
 		}
 	});
 
+	$("#ohjelma-link").click(function(e) {
+		e.preventDefault()
+
+		if ($("#ohjelma").length === 0) {
+
+		    window.location.href = "http://vire.lahdensiniset.net/pestit/#ohjelma";
+		    
+		} else {
+
+			$('html, body').animate({
+				scrollTop: $("#ohjelma").offset().top
+			}, 1000);
+
+		}
+	});
+
+	$("#ikakausi-link").click(function(e) {
+		e.preventDefault()
+
+		if ($("#ikakausi").length === 0) {
+
+		    window.location.href = "http://vire.lahdensiniset.net/pestit/#ikakausi";
+		    
+		} else {
+
+			$('html, body').animate({
+				scrollTop: $("#ikakausi").offset().top
+			}, 1000);
+
+		}
+	});
+
+	$("#ruoka-link").click(function(e) {
+		e.preventDefault()
+
+		if ($("#ruoka").length === 0) {
+
+		    window.location.href = "http://vire.lahdensiniset.net/pestit/#ruoka";
+		    
+		} else {
+
+			$('html, body').animate({
+				scrollTop: $("#ruoka").offset().top
+			}, 1000);
+
+		}
+	});
+
+	$("#huolto-link").click(function(e) {
+		e.preventDefault()
+
+		if ($("#huolto").length === 0) {
+
+		    window.location.href = "http://vire.lahdensiniset.net/pestit/#huolto";
+		    
+		} else {
+
+			$('html, body').animate({
+				scrollTop: $("#huolto").offset().top
+			}, 1000);
+
+		}
+	});
+
+	$("#vene-link").click(function(e) {
+		e.preventDefault()
+
+		if ($("#vene").length === 0) {
+
+		    window.location.href = "http://vire.lahdensiniset.net/pestit/#vene";
+		    
+		} else {
+
+			$('html, body').animate({
+				scrollTop: $("#vene").offset().top
+			}, 1000);
+
+		}
+	});
+
+	$("#juhlavuosi-link").click(function(e) {
+		e.preventDefault()
+
+		if ($("#juhlavuosi").length === 0) {
+
+		    window.location.href = "http://vire.lahdensiniset.net/pestit/#juhlavuosi";
+		    
+		} else {
+
+			$('html, body').animate({
+				scrollTop: $("#juhlavuosi").offset().top
+			}, 1000);
+
+		}
+	});
+
+
+
+
 	$(".summary").click( function() {
 
 		$( this.closest(".details") ).find(".job-contents:first").toggle(500);
